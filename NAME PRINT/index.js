fc47d79bd69af050d74244b31754346bba0d8d8b
M Rizwan Uddin <125825286+MRizwanRohan@users.noreply.github.com>
2023-10-02 00:35:53 +0600

    Add files via upload

---
+++ NAME PRINT/index.js
@@ -0,0 +1,11 @@
+var firstName,lastName,fullName;
+
+firstName = prompt("What is your first name?");
+lastName = prompt("What is your last name?");
+fullName = (firstName + " " + lastName);
+
+fullName = fullName.toUpperCase(); 
+
+alert("Hello, " + fullName + "!");
+
+alert("I hope you enjoyed your day!"); 
