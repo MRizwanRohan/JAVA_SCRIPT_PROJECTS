1640732e59755d7efb6f488bbf2c13c5c4a6cf59
M Rizwan Uddin <125825286+MRizwanRohan@users.noreply.github.com>
2023-10-02 00:35:24 +0600

    Add files via upload

---
+++ SIMPLE_SUM/index.js
@@ -0,0 +1,13 @@
+
+var num1,num2,sum,sub;
+
+num1=parseInt(prompt("Please enter 1st number ="));
+num2=parseInt(prompt("Enter 2nd number  = "));
+
+
+sum = num1 + num2;
+sub = num1 - num2;
+
+alert("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+alert("The difference of " + num1 + " and " + num2 + " is " + sub);
